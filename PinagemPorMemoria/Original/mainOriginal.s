79f8e1478b133a41ea67ab9944bb411573879ce3
Silas Silva <silas@github.com>
2023-03-13 11:28:52 -0300

    testes

--- PinagemPorMemoria/Original/mainOriginal.s
+++ PinagemPorMemoria/Original/mainOriginal.s
@@ -13,32 +13,29 @@ _start:
     mapMem
     nanoSleep
     GPIODirectionOut pin17
-    
-    @GPIODirectionOut pin27
-    @GPIODirectionOut pin22
+    GPIODirectionOut pin27
+    GPIODirectionOut pin22
     @ set up a loop counter for 10 iterations
-    @mov r6, #10
+    mov r6, #10
 
 loop: 
     GPIOTurnOn pin17
-    b loop
-    
-    @nanoSleep
-    @GPIOTurnOff pin17
-    @GPIOTurnOn pin27
-    @nanoSleep
-    @GPIOTurnOff pin27
-    @GPIOTurnOn pin22
-    @nanoSleep
+    nanoSleep
+    GPIOTurnOff pin17
+    GPIOTurnOn pin27
+    nanoSleep
+    GPIOTurnOff pin27
+    GPIOTurnOn pin22
+    nanoSleep
 
-@brk1:
-    @GPIOTurnOff pin22
+brk1:
+    GPIOTurnOff pin22
     @decrement loop counter and see if we loop
-    @subs r6, #1
+    subs r6, #1
     @ If we haven't counted down to 0 then loop
-    @bne loop
+    bne loop
 
-@_end: 
-@    mov R0, #0 @ Use 0 return code
-@    mov R7, #1 @ Command code 1 terms
-@    svc 0 @ Linux command to terminate
+_end: 
+    mov R0, #0 @ Use 0 return code
+    mov R7, #1 @ Command code 1 terms
+    svc 0 @ Linux command to terminate
