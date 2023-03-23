543c7e9e201308f157653d7cc448c1bd3bbf036c
Silas Silva <silas@github.com>
2023-03-23 17:14:03 -0300

    att

--- timer/macros_gerais.s
+++ timer/macros_gerais.s
@@ -1,8 +1,10 @@
-
+@ ============================================== DEVMEM E MAPEAMENTO ==============================================
+@
 @r0: Carregue o endereço da string com o nome do arquivo a ser aberto.
 @r1: Carregue o modo de abertura do arquivo. O modo é um valor inteiro que indica como o arquivo deve ser aberto (por exemplo, leitura, escrita, criação, etc.).
 @r7: Carregue o número da chamada do sistema, o valor é 5.
 @Chame a instrução svc 0 para executar a chamada do sistema.
+@
 .macro openDevmem
     ldr r0, =devmem       @Caminho do devmem, /dev/mem
 	mov r1, #2            @
@@ -12,6 +14,7 @@
 .endm
 
 
+@
 @r0: passar 0 para deixar o Linux escolher um endereço virtual adequado.
 @r1: passar o tamanho da memória que deseja mapear.
 @r2: passar as permissões que deseja atribuir ao mapeamento (por exemplo, PROT_READ + PROT_WRITE para permissão de leitura e escrita).
@@ -19,6 +22,7 @@
 @r4: passar o descritor de arquivo para /dev/mem.
 @r5: passar o endereço físico da GPIO dividido 0x1000.
 @ O endereço virtual será retornado em r0, armazene em outro registrador para uso futuro
+@
 .macro mapMem 
     mov r4, r0            @Mover descritor do arquivo de r0 para r4
     mov r0, #0            @Deixar o SO escolher o mapeamento
@@ -33,9 +37,12 @@
 .endm
 
 
-@ r0: Tempo de entrada para esperar em segundos e nanossegundos.
-@ r1: É o tempo restante para esperar se for interrompido
+@ =================================================== NANOSLEEP ===================================================
+@
+@r0: Tempo de entrada para esperar em segundos e nanossegundos.
+@r1: É o tempo restante para esperar se for interrompido
 @r7: Chama o ponto de entrada nanosleep do Linux, que é a função 162.
+@
 .macro nanoSleep
     ldr r0, =timespecnano
     ldr r1, =timespecnano
@@ -44,18 +51,58 @@
 .endm
 
 
+
+
+@ ======================================== DIVISÂO E SEPARAÇÂO DE DIGITOS ========================================
+@
+@ r0 - dividendo
+@ r1 - divisor
+@ r2 - quociente
+@ r3 - resto
+@
 .macro mod_division num1, num2
-    @ r0 - dividendo
-    @ r1 - divisor
-    @ r2 - quociente
-    @ r3 - resto
-    
     mov r0, \num1                 @Numero a ser dividido
     mov r1, \num2                 @Numero para dividir
     sdiv r2, r0, r1               @Numero divido
     
     @Resto
     mul r3, r1, r2                @Quociente x Divisor
-    sub r3, r0, r3                @Dividendo - (Quociente x Divisor)
+    sub r0, r0, r3                @Dividendo - (Quociente x Divisor)
+.endm
 
+
+@
+@Pegar 6 Digitos de um numero 
+@r3: Unidade
+@r4: Dezena
+@r5: Centena
+@r6: Milhar
+@r7: Dezena de Milhar
+@r9: Centena de Milhar
+@
+.macro num_para_digitos num
+    mov r1, \num
+    mov r2, #10
+    mod_division r3, r2     @ Unidade
+    mov r3, r0              @ Unidade
+
+    sdiv r4, #10            @ Dezena
+    mod_division r4, r2     @ Dezena
+    mov r4, r0              @ Dezena
+
+    sdiv r5, #100           @ Centena
+    mod_division r5, r2     @ Centena
+    mov r5, r0              @ Centena
+
+    sdiv r6, #1000          @ Milhar
+    mod_division r6, r2     @ Milhar
+    mov r6, r0              @ Milhar
+
+    sdiv r7, #10000         @ Dezena Milhar
+    mod_division r7, r2     @ Dezena Milhar
+    mov r7, r0              @ Dezena Milhar
+
+    sdiv r9, #100000        @ Centena Milhar
+    mod_division r9, r2     @ Centena Milhar
+    mov r9, r0              @ Centena Milhar
 .endm
