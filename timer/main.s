2125fe51580026483185101cd6d49c2f8818551c
Silas Silva da Costa <66216800+silas-silva@users.noreply.github.com>
2023-03-30 17:14:36 -0300

    Update main.s

--- timer/main.s
+++ timer/main.s
@@ -12,12 +12,13 @@ _start:
     openDevmem      @abrir arquivo devmem
     mapMem          @Mapear
     movs r8, r0     @Jogar o mapeamento em r8
-    
+
     @ Setar os pinos do display como saida
     pins_display_saida
     
     @Ligar display
-    @inicializar_display
+    inicializar_display
+
 
     @Timer
     @b loop
@@ -44,13 +45,16 @@ end:
     gpioaddr: .word 0x1C20    @0x01C20800 / 0x1000 (4096)   @Endereço base do GPIO / 0x1000
     padraoPin: .word 0x77777777
     padraoSaidaPin: .word 0x00000000
-    pins_d7_d6_saida: .word 0x11777777 @0001 0001 0111 0111 0111
-    pins_d5_d4_saida: .word 0x77777711
-    pins_E_RS_saida: .word 0x77777177
-    enable_saida: .word 0x00040000  @ 0000 0000 0000 0100 0000 0000 0000 0000, só fazer um or para acender o bit 18
     numContar: .word 0xFF 
     second: .word 1                 @ 1 segundo
     timeZero: .word 0
     timeZeroMili: .word 000000000
+    time_1_mili: .word 1000000
+    time_5_mili: .word 5000000
+    time_15_mili: .word 15000000
+    time_1_micro: .word 1000
+    time_100_micro: .word 100000
+    time_1_nano: .word 1000000000
+    time_100_nano: .word 1000000000
     timespecnano5: .word 5000000    @ 5 milissegundos
 		timespecnano120: .word 120000   @ 120 microssegundos
