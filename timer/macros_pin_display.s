1e4019c23b0e02e9f9e2a89595acb41648924074
Silas Silva da Costa <66216800+silas-silva@users.noreply.github.com>
2023-03-31 11:44:50 -0300

    Update macros_pin_display.s

--- timer/macros_pin_display.s
+++ timer/macros_pin_display.s
@@ -11,6 +11,7 @@
 
 
 
+
 @ ====================================== Pinos do Display como saida ======================================
 @
 .macro pins_display_saida
@@ -52,172 +53,32 @@
 .endm
 
 
-@ =========================================== Inicializar display ===========================================
+
+
+@
+@=========================================== Controladores dos Pinos DB4 a DB7, RS e Enable ===========================================
+@
+@ Todos os controladores, esperam o valor de configuração do pino no r7
+@ r7: esperam o valor de configuração do pino
 @
 
-.macro inicializar_display
-    function_Set
 
-    .ltorg
-    
-    set_display  0, 0, 0, 1, 0
-    set_enable
-
-    set_display  0, 0, 0, 1, 0
-    set_enable
-
-    set_display  0, 0, 0, 1, 1
-    set_enable
-
-
-
-    set_display  0, 0, 0, 0, 0
-    set_enable
-
-    set_display  0, 0, 0, 0, 0
-    set_enable
-
-    set_display  0, 0, 0, 0, 0
-    set_enable
-
-    set_display  0, 0, 0, 0, 1
-    set_enable
-
-    set_display  0, 0, 0, 0, 0
-    set_enable
-
-    set_display  0, 0, 1, 1, 0
-    set_enable
-
-    .ltorg
-.endm
-
-@ ======================================================================================
 @
-
-.macro setH
-    set_display 1, 0, 1, 0, 0
-    set_enable
-
-    set_display 1, 1, 0, 0, 0
-    set_enable
-.endm
-
-@ ======================================================================================
+@ Db4 On
 @
-
-.macro clear
-    set_display 0, 0, 0, 0, 0
-    set_enable
-
-    set_display 0, 0, 0, 0, 1
-    set_enable
-.endm
-
-@ ======================================================================================
-@
-
-.macro entry_mode_Set
-    set_display 0, 0, 0, 0, 0
-    set_enable
-
-    set_display 0, 0, 1, 1, 0
-    set_enable
-.endm
-
-@ ======================================================================================
-@
-.macro display_on_off
-    set_display 0, 0, 0, 0, 0
-    set_enable
-
-    set_display 0, 1, 1, 1, 0
-    set_enable
-.endm
-
-
-@ ======================================================================================
-@
-.macro function_Set
-        
-    @Primeiros 3 comandos
-    set_display 0, 0, 0, 1, 1
-    set_enable_1
-    nanoSleep timeZero, time_5_mili
-    set_enable_0
-
-    set_display 0, 0, 0, 1, 1
-    set_enable_1
-    nanoSleep timeZero, time_100_micro
-    set_enable_0
-
-    set_display  0, 0, 0, 1, 1
-    set_enable
-
-    set_display  0, 0, 0, 1, 0
-    set_enable
-
-.endm
-
-
-@ ======================================================================================
-@
-.macro set_enable
-    set_enable_0
-    set_enable_1
-    nanoSleep timeZero, time_1_mili
-    set_enable_0
-.endm
-
-@ ====================================== controle enable ======================================
-@Setar enable como saida logica alta (1)
-.macro set_enable_1
-    ldr r6, [r8, #0x810] @PA_dat
-    mov r7, #0x1
-    lsl r7, r7, #18
+.macro db4_on
+    ldr r6, [r8, #0x8E8] @PG_dat
+    lsl r7, r7, #8
     orr r6, r6, r7
-    str r6, [r8, #0x810]
+    str r6, [r8, #0x8E8]
 .endm
 
-@ ======================================================================================
+
 @
-@Setar enable como saida logica baixa (0)
-.macro set_enable_0
-    ldr r6, [r8, #0x810] @PA_dat
-    mov r7, #0x1
-    lsl r7, r7, #18
-    bic r6, r6, r7
-    str r6, [r8, #0x810]
-.endm
-
-@ ============================================== Setar display ==============================================
+@ Db4 Off
 @
-.macro set_display RS, DB7, DB6, DB5, DB4
-    
-    mov r7, #\DB4
-    controller_d4
-
-    mov r7, #\DB5
-    controller_d5
-
-    mov r7, #\DB6
-    controller_d6
-
-    mov r7, #\DB7
-    controller_d7
-    
-    mov r11, #0x0
-    add r11, #\RS
-    lsl r11, r11, #2
-    str r11, [r8, #0x810]
-
-.endm  @Configuração de pinos para o led
-
-
-
-@=====================================================================================================================
-.macro controller_d4
-    ldr r6, [r8, #0x8E8] @PA_dat
+.macro db4_off
+    ldr r6, [r8, #0x8E8] @PG_dat
     lsl r7, r7, #8
     orr r6, r6, r7
     str r6, [r8, #0x8E8]
@@ -225,37 +86,406 @@
 
 
 
-.macro controller_d5
-    ldr r6, [r8, #0x8E8] @PA_dat
+@
+@ Db5 On
+@
+.macro db5_on
+    ldr r6, [r8, #0x8E8] @PG_dat
     lsl r7, r7, #9
     orr r6, r6, r7
     str r6, [r8, #0x8E8]
 .endm
 
 
-.macro controller_d6
-    ldr r6, [r8, #0x8E8] @PA_dat
+@
+@ Db5 Off
+@
+.macro db5_off
+    ldr r6, [r8, #0x8E8] @PG_dat
+    lsl r7, r7, #9
+    bic r6, r6, r7
+    str r6, [r8, #0x8E8]
+.endm
+
+
+@
+@ Db6 On
+@
+.macro db6_on
+    ldr r6, [r8, #0x8E8] @PG_dat
     lsl r7, r7, #6
     orr r6, r6, r7
     str r6, [r8, #0x8E8]
 .endm
 
-.macro controller_d7
-    ldr r6, [r8, #0x8E8] @PA_dat
+
+@
+@ Db6 Off
+@
+.macro db6_off
+    ldr r6, [r8, #0x8E8] @PG_dat
+    lsl r7, r7, #6
+    bic r6, r6, r7
+    str r6, [r8, #0x8E8]
+.endm
+
+
+@
+@ Db7 on
+@
+.macro db7_on
+    ldr r6, [r8, #0x8E8] @PG_dat
     lsl r7, r7, #7
     orr r6, r6, r7
     str r6, [r8, #0x8E8]
 .endm
 
 
+@
+@ Db7 Off
+@
+.macro db7_off
+    ldr r6, [r8, #0x8E8] @PG_dat
+    lsl r7, r7, #7
+    bic r6, r6, r7
+    str r6, [r8, #0x8E8]
+.endm
 
 
-@PG07 -> d7   -> PG07 : Pino -> 30:28 : Bits de configuração -> 0xD8 : Offset
-@PG06 -> d6   -> PG06 : Pino -> 26:24 : Bits de configuração -> 0xD8 : Offset
-@PG09 -> d5   -> PG09 : Pino -> 06:04 : Bits de configuração -> 0xDC : Offset
-@PG08 -> d4   -> PG08 : Pino -> 02:00 : Bits de configuração -> 0xDC : Offset
-@ PG_DATA -> 0xE8 : Offset -> Bits para mandar sinal alto ou baixo pro pino
+@
+@ RS On
+@
+.macro rs_on
+    ldr r6, [r8, #0x810] @PA_dat
+    mov r7, #1
+    lsl r7, r7, #2
+    orr r6, r6, r7
+    str r6, [r8, #0x810]
+.endm
 
-@PA18 -> E    -> PA18 : Pino -> 10:08 : Bits de configuração -> 0x08 : Offset
-@PA02 -> RS   -> PA02 : Pino -> 10:08 : Bits de configuração -> 0x00 : Offset
-@ PA_DATA -> 0x10 : Offset -> Bits para mandar sinal alto ou baixo pro pino
+
+@
+@ RS Off
+@
+.macro rs_off
+    ldr r6, [r8, #0x810] @PA_dat
+    mov r7, #1
+    lsl r7, r7, #2
+    orr r6, r6, r7
+    str r6, [r8, #0x810]
+.endm
+
+
+@
+@ Enable On
+@
+.macro e_on
+    ldr r6, [r8, #0x810] @PA_dat
+    mov r7, #0x1
+    lsl r7, r7, #18
+    orr r6, r6, r7
+    str r6, [r8, #0x810]
+.endm
+
+
+@
+@ Enable Off
+@
+.macro e_off
+    ldr r6, [r8, #0x810] @PA_dat
+    mov r7, #0x1
+    lsl r7, r7, #18
+    bic r6, r6, r7
+    str r6, [r8, #0x810]
+.endm
+
+
+
+
+@
+@ ================================================== enable ==================================================
+@
+@ Depois de colocar os dados no pino do db4 a db7, chamar o enable, para o LCD ler os bits
+@
+.macro enable
+    e_off
+    e_on
+    nanoSleep timeZero, time_1_mili
+    e_on
+.endm
+
+
+
+
+@
+@ ============================================== Clear Display ==============================================
+@ 
+@ Limpar o display
+@
+
+.macro clear_display
+    @rs, d7, d6, d5, d4
+    @set_display 0, 0, 0, 0, 0
+    rs_off
+    db7_off
+    db6_off
+    db5_off
+    db4_off
+    enable
+
+    @set_display 0, 0, 0, 0, 1
+    rs_off
+    db7_off
+    db6_off
+    db5_off
+    db4_on
+    enable
+.endm
+
+
+
+
+@
+@ ================================================== Display ON/OFF ==================================================
+@
+.macro display_on
+    @rs, d7, d6, d5, d4
+    @set_display 0, 0, 0, 0, 0
+    rs_off
+    db7_off
+    db6_off
+    db5_off
+    db4_off
+    enable
+
+    @set_display 0, 1, 1, 1, 1  @Ultimo parametro, faz cursor piscar, penultimo faz o cursor ligar, e o do lado é para ligar o lcd
+    rs_off
+    db7_on
+    db6_on
+    db5_on
+    db4_on
+    enable
+.endm
+
+
+.macro display_off
+    @rs, d7, d6, d5, d4
+    @set_display 0, 0, 0, 0, 0
+    rs_off
+    db7_off
+    db6_off
+    db5_off
+    db4_off
+    enable
+
+    @set_display 0, 1, 0, 0, 0  @Ultimo parametro, faz cursor piscar, penultimo faz o cursor ligar, e o do lado é para ligar o lcd
+    rs_off
+    db7_on
+    db6_off
+    db5_off
+    db4_off
+    enable
+.endm
+
+
+
+
+@
+@ ======================================= Modo set de dados ===============================================
+@
+@ Configura o display para receber um dado, escrever e deslocar o cursor para a proxima posição
+@
+.macro entry_mode_set
+    @rs, d7, d6, d5, d4
+    @set_display 0, 0, 0, 0, 0
+    rs_off
+    db7_off
+    db6_off
+    db5_off
+    db4_off
+    enable
+
+    @set_display 0, 0, 1, 1, 0
+    rs_off
+    db7_off
+    db6_on
+    db5_on
+    db4_off
+    enable
+.endm
+
+
+
+
+@
+@ ========================================================= Set Função 4 bits =========================================================
+@
+.macro function_set
+    @rs, d7, d6, d5, d4
+    
+    @set_display 0, 0, 0, 1, 1
+    rs_off
+    db7_off
+    db6_off
+    db5_on
+    db4_on
+    e_on
+    nanoSleep timeZero, time_4_mili
+    e_off
+
+
+    @set_display 0, 0, 0, 1, 1
+    rs_off
+    db7_off
+    db6_off
+    db5_on
+    db4_on
+    e_on
+    nanoSleep timeZero, time_100_micro
+    e_off
+
+    @set_display  0, 0, 0, 1, 0
+    rs_off
+    db7_off
+    db6_off
+    db5_on
+    db4_on
+    enable
+.endm
+
+
+
+
+@ ===================================================== Setar Letra H =====================================================
+@
+
+.macro set_h
+    @rs, d7, d6, d5, d4
+    @set_display 1, 0, 1, 0, 0
+    rs_on
+    db7_off
+    db6_on
+    db5_off
+    db4_off
+    enable
+
+    @set_display 1, 1, 0, 0, 0
+    rs_on
+    db7_on
+    db6_off
+    db5_off
+    db4_off
+    enable
+.endm
+
+
+
+
+@ =========================================== Inicializar display ===========================================
+@
+.macro inicializar_display
+    e_off
+    @Iniciar modo 4 bits
+    function_Set
+
+    .ltorg
+    
+    @rs, d7, d6, d5, d4
+    @set_display 0, 0, 0, 1, 0
+    rs_off
+    db7_off
+    db6_off
+    db5_on
+    db4_off
+    enable
+
+    @rs, d7, d6, d5, d4
+    @set_display 0, 0, 0, 1, 0
+    rs_off
+    db7_off
+    db6_off
+    db5_on
+    db4_off
+    enable
+
+    @rs, d7, d6, d5, d4
+    @set_display 0, 0, 0, 0, 0
+    rs_off
+    db7_off
+    db6_off
+    db5_on
+    db4_off
+    enable
+
+
+    @rs, d7, d6, d5, d4
+    @set_display 0, 0, 0, 0, 0
+    rs_off
+    db7_off
+    db6_off
+    db5_off
+    db4_off
+    enable
+
+
+    @rs, d7, d6, d5, d4
+    @set_display 0, 1, 0, 0, 0
+    rs_off
+    db7_on
+    db6_off
+    db5_off
+    db4_off
+    enable
+
+    @rs, d7, d6, d5, d4
+    @set_display 0, 0, 0, 0, 0
+    rs_off
+    db7_off
+    db6_off
+    db5_off
+    db4_off
+    enable
+
+    @rs, d7, d6, d5, d4
+    @set_display 0, 0, 0, 0, 1
+    rs_off
+    db7_off
+    db6_off
+    db5_off
+    db4_on
+    enable
+
+    @rs, d7, d6, d5, d4
+    @set_display 0, 0, 0, 0, 0
+    rs_off
+    db7_off
+    db6_off
+    db5_off
+    db4_off
+    enable
+
+    @rs, d7, d6, d5, d4
+    @set_display 0, 0, 1, 1, 0
+    rs_off
+    db7_off
+    db6_on
+    db5_on
+    db4_off
+    enable
+
+
+
+    @display_on
+    
+    @Ligar display
+    @display_off
+
+    @Modo de setar
+    @entry_mode_set
+
+    @set h
+    @set_h
+    @clear_display
+
+    .ltorg
+.endm
